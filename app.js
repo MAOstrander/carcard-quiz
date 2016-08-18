85ce055c232da470e47e0cfe12f663c292e42702
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-08-18 10:25:24 -0500

    trying to quickly catch up with the class

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+function populatePage(inventory) {
+  var outputDiv = document.getElementById("output")
+    inventory.forEach(function(cars) {
+    outputDiv.innerHTML += `
+    <div class="col-md-4 carCard" style="border-color: ${cars.color}">
+      <h1 class="display-2">Make: ${cars.make}</h1>
+      <p>Model: ${cars.model}</p>
+      <p>Year: ${cars.year}</p>
+      <p>Price: ${cars.price}</p>
+      <p>Color: ${cars.color}</p>
+      <p>Available: ${cars.purchased}</p>
+      <p>Description: ${cars.description}</p>
+    </div>`
+    })
 
+  }
 
-CarLot.loadInventory().then(function(dataStuff){
-  console.log(dataStuff)
+CarLot.loadInventory()
+.then(function(dataStuff){
+  populatePage(dataStuff)
+
 })
